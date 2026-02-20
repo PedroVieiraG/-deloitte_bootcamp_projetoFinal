1e9efbb4d68ce937b32754adfc8f14fcc0c8bb0c
Pedro Vinicius <pedrovvgl.com>
2026-02-20 14:24:20 -0300

    correção de erros

--- MoniEquipamentosPesados/Program.cs
+++ MoniEquipamentosPesados/Program.cs
@@ -3,16 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Padrão restaurado: Enums serão lidos e enviados como números (0, 1, 2...)
 builder.Services.AddControllers();
 
-// Configuração do PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Verifica e aplica migrations pendentes ao iniciar (útil para o Docker)
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
